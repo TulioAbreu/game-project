488140b93384b346c862446c90ffa687860429b7
TulioAbreu <marco-tulioabreu1@hotmail.com>
2020-02-22 11:30:36 -0300

    Move window implementation to a class

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,45 @@ public:
     bool getIsRunning() const { return mIsRunning; }
 };
 
+class Window {
+    sf::RenderWindow mWindow;
+    sf::Event mEvent;
+    sf::RectangleShape mRect;
+
+public:
+    Window(int width, int height, std::string title) : mWindow(sf::VideoMode(width, height), title) {}
+    virtual ~Window() = default;
+
+    void handleWindowEvents() {
+        while (mWindow.pollEvent(mEvent)) {
+            if (mEvent.type == sf::Event::Closed) {
+                // TODO: Maybe add some kind of callback to window class?
+                mWindow.close();
+            }
+        }
+    }
+
+    void clear() { mWindow.clear(); }
+
+    void display() { mWindow.display(); }
+
+    bool isOpen() { return mWindow.isOpen(); }
+};
+
+struct Rectangle {
+    float width, height;
+    float positionX, positionY;
+};
+
+
 int main() {
     Game game;
     game.setIsRunning(true);
 
-    using namespace sf;
-    RenderWindow window (VideoMode(640, 480), "GameProject");
+    Window window (640, 480, "GameProject");
 
-    while (game.getIsRunning()) {
-        Event windowEvent;
-        while (window.pollEvent(windowEvent)) {
-            if (windowEvent.type == Event::Closed) {
-                game.setIsRunning(false);
-            }
-        }
+    while (game.getIsRunning() && window.isOpen()) {
+        window.handleWindowEvents();
 
         window.clear();
         window.display();
