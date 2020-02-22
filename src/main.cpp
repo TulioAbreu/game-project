c82f8e05d5061ffeb36be0f252e0b40d8d4df8e4
TulioAbreu <marco-tulioabreu1@hotmail.com>
2020-02-22 10:52:14 -0300

    Implement simple SFML window

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
-using namespace std;
+#include <SFML/Graphics.hpp>
+
+class Game {
+    bool mIsRunning;
+
+public:
+    Game() = default;
+    virtual ~Game() = default;
+
+    void setIsRunning(const bool value) { mIsRunning = value; }
+    bool getIsRunning() const { return mIsRunning; }
+};
 
 int main() {
-    cout << "Hello, World!" << endl;
+    Game game;
+    game.setIsRunning(true);
+
+    using namespace sf;
+    RenderWindow window (VideoMode(640, 480), "GameProject");
+
+    while (game.getIsRunning()) {
+        Event windowEvent;
+        while (window.pollEvent(windowEvent)) {
+            if (windowEvent.type == Event::Closed) {
+                game.setIsRunning(false);
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
     return 0;
 }
