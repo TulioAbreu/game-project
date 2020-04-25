0fd1880d28a515f5dfb99176cfb500c669256640
MarcoAbreu <marco.abreu@melhorplano.net>
2020-04-24 23:32:23 -0300

    Introduce log.h

---
+++ src/log.hpp
@@ -0,0 +1,13 @@
+#ifndef LOG_HPP
+#define LOG_HPP
+
+#include <cstdio>
+#include <string>
+#include <iostream>
+
+std::string ERROR_PREFIX = "\033[1;31m\[ERROR]\033[0m ";
+std::string WARNING_PREFIX = "\033[1;33m\[WARNING]\033[0m ";
+std::string LOG_PREFIX = "\033[1;37m\[LOG]\033[0m ";
+
+
+#endif
