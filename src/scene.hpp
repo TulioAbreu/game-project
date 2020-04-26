206d1e15c138dfa3d259a3a3ebde086690d61b82
MarcoAbreu <marco.abreu@melhorplano.net>
2020-04-26 11:44:22 -0300

    Implement simple scene file reader

--- src/scene.hpp
+++ src/scene.hpp
@@ -3,18 +3,68 @@
 
 #include "entity-container.hpp"
 #include "entity.hpp"
+#include "log.hpp"
+
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::stringstream ss(s);
+    std::string item;
+    std::vector<std::string> elems;
+    while (std::getline(ss, item, delim)) {
+        // elems.push_back(item);
+        elems.push_back(std::move(item)); // if C++11 (based on comment from @mchiasson)
+    }
+    return elems;
+}
 
 class Scene {
     private:
     Entities mEntities;
 
     void readSceneFile(std::string filepath) {
-        mEntities.add(Entity(200, 200, 0, 0));
-        mEntities.add(Entity(10, 10, 100, 100));
-        mEntities.add(Entity(10, 10, -100, -100));
+        std::ifstream file (filepath);
+        if (!file.is_open()) {
+            std::cout << ERROR_PREFIX << "Could not open file => " << filepath << std::endl;
+            return;
+        }
+        std::string sceneStr((std::istreambuf_iterator<char>(file)),
+                               std::istreambuf_iterator<char>());
+        file.close();
 
-        mEntities.add(Entity(10, 10, 310, 0));
-        mEntities.add(Entity(50, 50, 200, 300));
+        auto sceneStrVec = split(sceneStr, '\n');
+        for (auto sceneStrElement : sceneStrVec) {
+            auto nameAttributesVec = split(sceneStrElement, '>');
+            if (nameAttributesVec.size() != 2) {
+                continue;
+            }
+
+            const std::string elementName = nameAttributesVec[0];
+            const std::string allAttributesStr = nameAttributesVec[1];
+
+            Vector2f size, position;
+
+            auto attributesVec = split(allAttributesStr, ';');
+            for (auto attribute : attributesVec) {
+                auto attributeValueVec = split(attribute, ':');
+                
+                const std::string attributeTitleStr = attributeValueVec[0];
+                const std::string attributeValueStr = attributeValueVec[1];
+
+                if (attributeTitleStr == "size") {
+                    auto values = split(attributeValueStr, ' ');
+                    size = {std::stof(values[0]), std::stof(values[1])};
+                }
+                else if (attributeTitleStr == "position") {
+                    auto values = split(attributeValueStr, ' ');
+                    position = {std::stof(values[0]), std::stof(values[1])};
+                }
+            }
+            mEntities.add(Entity(size.x, size.y, position.x, position.y));
+        }
     }
     public:
     Scene(std::string filepath) {
