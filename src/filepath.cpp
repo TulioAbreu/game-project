db8e8e847fa04038b729d4f89d0bcf8cf3bdfbad
Marco Tulio Oliveira Abreu <marco-tulioabreu@hotmail.com>
2020-05-23 18:45:36 -0300

    Build was moved to root folder

--- src/filepath.cpp
+++ src/filepath.cpp
@@ -1,7 +1,7 @@
 #include "filepath.hpp"
 
 std::string Path(std::string relativeFilepath) {
-    const std::string FILEPATH_ROOT ("../");
+    const std::string FILEPATH_ROOT ("./");
     return FILEPATH_ROOT + relativeFilepath;
 }
 
