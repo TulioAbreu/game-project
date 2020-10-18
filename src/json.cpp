f223a181d243b2725c009939881d4b990987f38b
TulioAbreu <marco-tulioabreu1@hotmail.com>
2020-10-18 09:13:10 -0300

    style: change spacing

--- src/json.cpp
+++ src/json.cpp
@@ -1,11 +1,12 @@
 #include "json.hpp"
 
-bool JSON::load(std::string filePath, json* jsonObject) {
+bool JSON::load(std::string filePath, json* jsonObject)
+{
     if (!jsonObject) {
         return false;
     }
 
-    std::fstream file (filePath);
+    std::fstream file(filePath);
     if (!file.is_open()) {
         return false;
     }
